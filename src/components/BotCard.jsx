7dff2ecc6d4b26e08ab4045ee5486cd73072faf1
Abiud-lang <abiud.kiprotich@student.moringaschool.com>
2024-08-10 00:53:31 +0300

    added A BotCard component to display individual bot

---
+++ src/components/BotCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const BotCard = () => {
+  return (
+    <div>
+         <img src={bot.avatar_url} alt={bot.name} />
+      <h3>{bot.name}</h3>
+      <p>Class: {bot.bot_class}</p>
+      <p>Health: {bot.health}</p>
+      <p>Damage: {bot.damage}</p>
+      <p>Armor: {bot.armor}</p>
+      <p>Catchphrase: {bot.catchphrase}</p>
+    </div>
+  )
+}
+
+export default BotCard
